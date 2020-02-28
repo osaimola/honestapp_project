922a28ecb71f7f338b0d0cf58ceab575e5b973e4
Osa <osaimola@gmail.com>
2020-02-27 23:05:34 -0700

    update admin title

--- honest/admin.py
+++ honest/admin.py
@@ -1,14 +1,18 @@
 from django.contrib import admin
 from honest.models import Category, Area, Person, UserProfile, Review
 
+admin.site.site_header = "Honestng Admin"
+admin.site.site_title = "Honestng Admin Area"
 
 # Register your models here.
+
+
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('category',)}
 
 
 class AreaAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('state'),}
+    prepopulated_fields = {'slug': ('state'), }
 
 
 admin.site.register(Category, CategoryAdmin)
